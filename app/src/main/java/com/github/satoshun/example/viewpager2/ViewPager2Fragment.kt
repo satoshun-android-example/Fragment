85611d5c168538cd8634ab7a0afda7a644f81c1e
SatoShun <shun.sato1@gmail.com>
2019-11-13 06:24:25 +0000

    add observe timing ViewModel + LiveData

--- app/src/main/java/com/github/satoshun/example/viewpager2/ViewPager2Fragment.kt
+++ app/src/main/java/com/github/satoshun/example/viewpager2/ViewPager2Fragment.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.github.satoshun.example.databinding.ViewPager2FragBinding
 
@@ -35,9 +39,15 @@ class ViewPager2ChildFragment : Fragment() {
 
   var userId: Int = 0
 
+  private val viewModel: ViewPager2ChildFragmentViewModel by viewModels()
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     println("onCreateView $userId")
     return TextView(requireContext()).apply { text = "$userId" }
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
   }
 
   override fun onStart() {
@@ -48,6 +58,10 @@ class ViewPager2ChildFragment : Fragment() {
   override fun onResume() {
     super.onResume()
     println("onResume $userId")
+
+    viewModel.data.observe(viewLifecycleOwner, Observer {
+      println("viewModel $it")
+    })
   }
 
   override fun onPause() {
@@ -58,5 +72,13 @@ class ViewPager2ChildFragment : Fragment() {
   override fun onStop() {
     super.onStop()
     println("onStop $userId")
+  }
+}
+
+class ViewPager2ChildFragmentViewModel : ViewModel() {
+  val data = MutableLiveData<Int>()
+
+  init {
+    data.value = 10
   }
 }
