63fab0029abac8ed49c60fb7d7015583462ba9ed
SatoShun <shun.sato1@gmail.com>
2019-11-15 04:41:39 +0000

    fragmetn

--- app/src/main/java/com/github/satoshun/example/viewpager2/ViewPager2Fragment.kt
+++ app/src/main/java/com/github/satoshun/example/viewpager2/ViewPager2Fragment.kt
@@ -1,16 +1,21 @@
 package com.github.satoshun.example.viewpager2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.github.satoshun.example.AppActivity
 import com.github.satoshun.example.databinding.ViewPager2FragBinding
 
 class ViewPager2Fragment : Fragment() {
@@ -22,10 +27,15 @@ class ViewPager2Fragment : Fragment() {
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    binding.viewPager.expandFragmentCacheSize()
     binding.viewPager.adapter = ViewPager2FragmentAdapter(this)
   }
 }
 
+private fun ViewPager2.expandFragmentCacheSize() {
+  val childView = this[0] as RecyclerView
+  childView.setItemViewCacheSize(5)
+}
 
 class ViewPager2FragmentAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
   override fun getItemCount(): Int = 100
@@ -41,6 +51,11 @@ class ViewPager2ChildFragment : Fragment() {
 
   private val viewModel: ViewPager2ChildFragmentViewModel by viewModels()
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    println("onCreate $userId")
+  }
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     println("onCreateView $userId")
     return TextView(requireContext()).apply { text = "$userId" }
@@ -48,6 +63,11 @@ class ViewPager2ChildFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    println("onViewCreated $userId")
+
+    view.setOnClickListener {
+      startActivity(Intent(requireContext(), AppActivity::class.java))
+    }
   }
 
   override fun onStart() {
@@ -72,6 +92,16 @@ class ViewPager2ChildFragment : Fragment() {
   override fun onStop() {
     super.onStop()
     println("onStop $userId")
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    println("onDestroyView $userId")
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    println("onDestroy $userId")
   }
 }
 
